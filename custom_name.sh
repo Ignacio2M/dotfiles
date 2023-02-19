f0c934a1a79f503b3246092f72dba0fbaf2c6cf2
Ignacio <ignacioUni12@gmail.com>
2023-02-19 19:45:51 +0100

    Add @ if git branch error

--- custom_name.sh
+++ custom_name.sh
@@ -6,7 +6,13 @@ Cyan="\[\e[1;36m\]"         # Cyan
 Red="\[\e[1;31m\]"         # Red
 
 parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/'
+
+    git branch &> /dev/null
+    if [ $? = 0 ]; then
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/[\1]/'
+    else
+        echo '@'
+    fi
 }
 
 PS1="${Purple}\u${Red}\$(parse_git_branch)${Cyan}\w ${Color_Off}\$ "
